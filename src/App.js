41daf2d19d057fd6697d04e605c45e1ee6cdaa01
Caleb Fung <caleb.t.fung@gmail.com>
2019-02-08 11:10:46 -0600

    routing

--- src/App.js
+++ src/App.js
@@ -3,39 +3,15 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { withRouter } from "react-router-dom";
 import Shell from './Shell';
 
-///////////////////////////////////////////////////////////////////////////////
-// React for GitHub Pages - https://github.com/rafrex/react-github-pages
-// ----------------------------------------------------------------------------
-// Takes the redirect query string created in 404.html, converts it back into
-// the correct url, then uses react-router to redirect to the correct url which
-// loads the respective routes and components. When there is a fresh page
-// load for a path that is defined with frontend routes, GitHub Pages will
-// return the custom 404.html page, which then redirects back to just the
-// base domain with a query string representing the attempted url, to which
-// GitHub Pages returns index.html. The single page react app is loaded,
-// this function is run, and the correct route is entered.
-function parseRedirectQuery(query) {
-  let redirectTo = {}
-
-  if (typeof query.pathname === 'string' && query.pathname !== '') {
-    redirectTo.pathname = query.pathname;
-  }
-
-  if (typeof query.query === 'string' && query.query !== '') {
-    let queryObject = {};
-    query.query.split('&').map(q => q.split('=')).forEach(arr => {
-      queryObject[arr[0]] = arr.slice(1).join('=');
-    })
-    redirectTo.query = queryObject;
-  }
-
-  if (typeof query.hash === 'string' && query.hash !== '') {
-    redirectTo.hash = `#${query.hash}`
-  }
-
-  return redirectTo;
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
-///////////////////////////////////////////////////////////////////////////////
 
 const theme = createMuiTheme({
   palette: {
@@ -61,8 +37,14 @@ class App extends Component {
 
   componentWillMount() {
     // Init route
-    console.log(parseRedirectQuery(this.props.location.pathname))
     this.setRoute(this.props.location.pathname);
+    var pathname = getParameterByName('pathname');
+    if (pathname == null) {
+      this.setRoute(this.props.location.pathname);
+    } else {
+      window.location.replace(process.env.PUBLIC_URL + pathname);
+      this.setRoute(pathname);
+    }
   }
 
   componentDidUpdate(prevProps) {
